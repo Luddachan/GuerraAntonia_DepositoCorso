214198f11694d71c1d9ae51a0ebb3078cc507160
Antonia Guerra <a.guerra12@studenti.unimol.it>
2025-11-17 11:56:13 +0100

    modifiche al nome
    modifiche

--- corso python/lunedì 17-11/Esempi_oggetti.py
+++ corso python/lunedì 17-11/Esempi_oggetti.py
@@ -8,10 +8,10 @@ class Automobile:
 		self.marca = marca
 		self.modello = modello
 	#metodo di istanza
-    def stampa_info(self):
-	    print("l'automobile è una", self.marca, self.modello)
-    def __str__(self):
-        return f"Automobile(nome ={self.nome}, eta={self.eta})"
+    # def stampa_info(self):
+	#     print("l'automobile è una", self.marca, self.modello)
+def __str__(self):
+    return f"Automobile(nome ={self.nome}, eta={self.eta})"
 
  
 #self è il nome dell'oggetto
@@ -22,3 +22,31 @@ auto2 = Automobile("BMW", "X3")
 auto1.stampa_info() #stampa l'automobile è una fiat 500
 auto2.stampa_info() #stampa l'uatomobile è una bmw x3
 
+
+#esempio 2
+
+class Calcolatrice:
+    @staticmethod
+    def somma (a, b):
+        return a+b
+#uso metodo statico senza creare un'istanza
+risultato =Calcolatrice.somma(5,3)
+
+print(risultato) #output 8
+
+class Contatore:
+    numero_istanze =0 #attributo di classe
+    
+    def __init__(self):
+         Contatore.numero_istanze += 1
+    
+    @classmethod
+    def mostra_numero_istanze(cls):
+        print(f"Sono state create {cls.numero_istanze} istanze")
+    
+    #creazione di alcune istanze
+c1= Contatore()
+c2= Contatore()
+
+Contatore.mostra_numero_istanze()
+#sono state create due istanze
