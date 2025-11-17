c8141e8b24e7c92cfced7ca1dcb1b00ff044ff27
Antonia Guerra <a.guerra12@studenti.unimol.it>
2025-11-17 11:07:58 +0100

    esercizio 2
    esercizio 2

---
+++ corso python/lunedì 17-11/Esercizio_2.py
@@ -0,0 +1,12 @@
+#definiamo la classe
+class Libro:
+    # costruttore: inizializza gli attributi
+    def __init__(self, titolo, autore, pagine):
+        #attributi di istanza
+        self.titolo = titolo
+        self.autore = autore
+        self.pagine = pagine
+    # metodo che restituisce una descrizione del libro
+    def descrizione(self):
+        # usa una f-string per creare la frase richiesta
+        return f"Libro '{self.titolo}' è di '{self.autore} con '{self.pagine} pagine."
