fdb848e26319ba5294331b19721e249d5aedba35
Antonia Guerra <a.guerra12@studenti.unimol.it>
2025-11-17 10:59:55 +0100

    esercizio 1
    esercizio 1

---
+++ corso python/lunedì 17-11/Esercizio_1.py
@@ -0,0 +1,15 @@
+class Punto:
+    def __init__ (self, x, y):
+        #metodi costruttore
+        self.x = x
+        self.y = y 
+    #definisci metodo muovi che prende 
+    def muovi (self, dx, sx):
+        self.dx += dx 
+        self.sx += sx
+    def distanza_da_origine(self):
+        
+#trovata su internet 
+#return math.sqrt(self.x**2 + self.y**2)
+        
+    
