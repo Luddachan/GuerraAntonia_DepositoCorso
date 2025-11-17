d1e6a12dc2fc3c1043da2dd0c4545a3ac86e6b5d
Antonia Guerra <a.guerra12@studenti.unimol.it>
2025-11-17 13:47:28 +0100

    esempi
    esempi

---
+++ corso python/lunedì 17-11/Esempi_dizionari.py
@@ -0,0 +1,28 @@
+""" studente = {
+	"nome": "Alice",
+	"età": 20,
+	"sesso": "Femmina"
+}
+
+print(studente["nome"])
+print(studente["età"])
+
+studente["città"]= "Roma" 
+print(studente)
+
+studente["età"] =21
+print(studente)
+
+print(studente.keys()) #output: dict keys ([Nome, età, sesso])
+print(studente.values()) #output: dict_values ([Alice, 20, Femmina]) """
+
+
+""" studente = {
+    "nome": Giovanni,
+    "età": 15,
+    "corso": Java 
+}
+
+sutudente = ["nome", "età", "corso"]
+for  """
+
