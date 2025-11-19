22ea5b4482d51a43751bf2fc31091a0f8bde36c0
Antonia Guerra <a.guerra12@studenti.unimol.it>
2025-11-19 13:00:12 +0100

    update
    update

--- corso python/mercoledì 19-11/Esercizio_gestione_studenti/studente.py
+++ corso python/mercoledì 19-11/Esercizio_gestione_studenti/studente.py
@@ -8,7 +8,7 @@ class Studente(Persona):
         super().__init__(nome, eta)
         self.voti = voti
     # metodo per calcolare la media dei voti    
-    def calcola_media(self):
+    def __calcola_media(self):
         # controllo se la lista dei voti è vuota
         if not self.voti:
             return 0
@@ -20,7 +20,7 @@ class Studente(Persona):
         # riscrive il metodo della classe padre
         presentazione_base = super().presentazione()
         # calcolo della media dei voti
-        media_voti = self.calcola_media()
+        media_voti = self.__calcola_media()
         # restituzione della presentazione completa
         # 2f per formattare la media con 2 cifre decimali
         return f"{presentazione_base} Sono uno studente e la mia media dei voti è {media_voti:.2f}."
