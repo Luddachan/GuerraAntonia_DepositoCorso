d71bf1a9cb7e87a7cf03a1f0fbaa5246a61c28f1
Antonia Guerra <a.guerra12@studenti.unimol.it>
2025-11-19 16:04:11 +0100

    esercizio parco auto

---
+++ corso python/mercoledì 19-11/Esercizio_veicolo/main.py
@@ -0,0 +1,18 @@
+from auto import Auto
+from furgone import Furgone
+from motocicletta import Motocicletta
+from gestore_parco import GestoreParcoVeicoli
+
+# Esempio di utilizzo delle classi definite
+
+parco = GestoreParcoVeicoli()
+
+a1 = Auto("Fiat", "Punto", 2010, 5)
+f1 = Furgone("Ford", "Transit", 2018, 1200)
+m1 = Motocicletta("Yamaha", "R6", 2020, "sportiva")
+
+parco.aggiungi_veicolo(a1)
+parco.aggiungi_veicolo(f1)
+parco.aggiungi_veicolo(m1)
+
+parco.lista_veicoli()
